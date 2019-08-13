7738a12be7bbc1fd34d32accaef2f981e2b0afd7
biewxw <gabrieldeoliveira.gdo23@gmail.com>
2019-08-13 13:57:32 -0300

    tests done

--- 4-arrays.test.js
+++ 4-arrays.test.js
@@ -22,57 +22,57 @@ describe('Arrays', function() {
     expect(array).toEqual([1]);
 
     array[1] = 2;
-    expect(array).toEqual([1, RESPOSTA]);
+    expect(array).toEqual([1, 2]);
 
     array.push(3);
-    expect(array).toEqual(RESPOSTA);
+    expect(array).toEqual([1, 2, 3]);
   });
 
   it('should understand array length', function () {
     var fourNumberArray = [1, 2, 3, 4];
+    expect(fourNumberArray.length).toBe(4);
 
-    expect(fourNumberArray.length).toBe(RESPOSTA);
     fourNumberArray.push(5, 6);
-    expect(fourNumberArray.length).toBe(RESPOSTA);
+    expect(fourNumberArray.length).toBe(6);
 
     var tenEmptyElementArray = new Array(10);
-    expect(tenEmptyElementArray.length).toBe(RESPOSTA);
+    expect(tenEmptyElementArray.length).toBe(10);
 
     tenEmptyElementArray.length = 5;
-    expect(tenEmptyElementArray.length).toBe(RESPOSTA);
+    expect(tenEmptyElementArray.length).toBe(5);
   });
 
   it('should slice arrays', function () {
     var array = ['peanut', 'butter', 'and', 'jelly'];
 
-    expect(array.slice(0, 1)).toEqual(RESPOSTA);
-    expect(array.slice(0, 2)).toEqual(RESPOSTA);
-    expect(array.slice(2, 2)).toEqual(RESPOSTA);
-    expect(array.slice(2, 20)).toEqual(RESPOSTA);
-    expect(array.slice(3, 0)).toEqual(RESPOSTA);
-    expect(array.slice(3, 100)).toEqual(RESPOSTA);
-    expect(array.slice(5, 1)).toEqual(RESPOSTA);
+    expect(array.slice(0, 1)).toEqual(['peanut']);
+    expect(array.slice(0, 2)).toEqual(['peanut', 'butter']);
+    expect(array.slice(2, 2)).toEqual([]);
+    expect(array.slice(2, 20)).toEqual(['and', 'jelly']);
+    expect(array.slice(3, 0)).toEqual([]);
+    expect(array.slice(3, 100)).toEqual(['jelly']);
+    expect(array.slice(5, 1)).toEqual([]);
   });
 
   it('should push and pop', function () {
     var array = [1, 2];
     array.push(3);
 
-    expect(array).toEqual(RESPOSTA);
+    expect(array).toEqual([1, 2, 3]);
 
     var poppedValue = array.pop();
-    expect(poppedValue).toBe(RESPOSTA);
-    expect(array).toEqual(RESPOSTA);
+    expect(poppedValue).toBe(3);
+    expect(array).toEqual([1, 2]);
   });
 
   it('should know about shifting arrays', function () {
     var array = [1, 2];
 
     array.unshift(3);
-    expect(array).toEqual(RESPOSTA);
+    expect(array).toEqual([3, 1, 2]);
 
     var shiftedValue = array.shift();
-    expect(shiftedValue).toEqual(RESPOSTA);
-    expect(array).toEqual(RESPOSTA);
+    expect(shiftedValue).toEqual(3);
+    expect(array).toEqual([1, 2]);
   });
 });
